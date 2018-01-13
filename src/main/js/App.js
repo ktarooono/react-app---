6b696550ac7f754ba59c3bd286b206639c4573d0
ktarooono <35089650+ktarooono@users.noreply.github.com>
2018-01-14 01:31:41 +0900

    環境構築完了

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,2 +1,33 @@
-import oono from "./oono.js";
+
+
+
+
+import React, { Component } from 'react';
+import {render} from "react-dom";
+
+
+class App extends Component {
+
+
+render(){
+	return (
+			<div>
+				大野 啓太郎
+			</div>
+			);
+
+
+}
+
+};
+
+console.log("app.js　読み込み成功しました！")
+
+render(<App/>,document.getElementById("react"));
+
+
+export default App;
+
+
+
 
